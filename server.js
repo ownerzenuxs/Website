cc1bc20093a2de7a0189c42999fc71742d2d8105
ownerzenuxs <iwantvpspls@gmail.com>
2025-01-20 11:22:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = 3000;
 
 // Password for login
-const PASSWORD = "admin123";
+const PASSWORD = "123";
 
 // Middleware to parse JSON and serve static files
 app.use(express.json());
